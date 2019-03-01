57d65f6e83ebae6007f1ddc41a8a0c5e331926c6
Vlad Shubnikov <vlad.shubnikov@gmail.com>
2019-03-01 13:08:34 +0300

    fix use Modes3d

--- src/demo/engine/Graphics3d.js
+++ src/demo/engine/Graphics3d.js
@@ -12,6 +12,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Modes3d from '../store/Modes3d';
+
 import VolumeRenderer3d from './VolumeRenderer3d'
 
 // ********************************************************
