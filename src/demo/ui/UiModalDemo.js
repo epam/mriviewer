7d9aa6f04cf675ae7e0e52b88468edc57aadd891
Vlad Shubnikov <vlad.shubnikov@gmail.com>
2019-05-28 12:51:13 +0300

    correct close button

--- src/demo/ui/UiModalDemo.js
+++ src/demo/ui/UiModalDemo.js
@@ -16,6 +16,28 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 import { Modal, Button } from 'react-bootstrap';
 
 // File list for demo
+
+//
+// OneDrive links
+//
+// ktx / ct_256..
+// https://epam-my.sharepoint.com/:u:/p/vladislav_shubnikov/EY01jASe0VBFgBAF8Y8Wb70BBnJMKnsR7Lttj-ikMLT25A?e=3PZffy
+// ktx / lungs_256....
+// https://epam-my.sharepoint.com/:u:/p/vladislav_shubnikov/EVvyy6uKRvxLnfOyWYSay-IBrKxDYmOm8b7OLPUzRkWY5g?e=T6i7YM
+// nifti / gm3_...
+// https://epam-my.sharepoint.com/:u:/p/vladislav_shubnikov/ERGfqrfwaONFo1GvgGdJkTwBypBudizNsKmYiQN5ucw-4A?e=rQtgJi
+//
+
+// Old refs
+//
+// https://med3web.opensource.epam.com/data/ktx/ct_256_256_256.ktx
+// https://med3web.opensource.epam.com/data/dicom/20121029/2_1.dcm
+// https://med3web.opensource.epam.com/data/hdr/set_mask.h
+// https://med3web.opensource.epam.com/data/ktx/brain.ktx
+// https://med3web.opensource.epam.com/data/ktx/brain_clipped.ktx
+
+//
+
 //
 // http://www.d-inter.ru/private/med3web/data/ktx/ct_256_256_256.ktx
 // http://www.d-inter.ru/private/med3web/data/ktx/lungs_256_256_256.ktx
@@ -95,55 +117,55 @@ class UiModalDemo extends React.Component {
 
     const jsxModalDemo = 
       <Modal show={stateVis} onHide={onHideFunc} >
-        <Modal.Title>
-          Load demo data
-        </Modal.Title>
         <Modal.Header closeButton>
-          <Modal.Body>
-
-            <Container>
-              <Row>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton0} >
-                    <Image src="images/thumb_lungs.png" alt="lungs" thumbnail />
-                  </Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton1} >
-                    <Image src="images/thumb_brain.png" alt="brain" thumbnail />
-                  </Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton2} >
-                    <Image src="images/thumb_gm3_512_512_165.png" alt="gm3" thumbnail />
-                  </Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton3} >
-                    <Image src="images/thumb_woman_pelvis.png" alt="woman_pelvis" thumbnail />
-                  </Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton4} >
-                    <Image src="images/thumb_ocb.png" alt="woman_pelvis" thumbnail />
-                  </Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton5} >
-                    <Image src="images/thumb_ct_256.png" alt="ct_256" thumbnail />
-                  </Button>
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button variant="light" onClick={this.onButton6} >
-                    <Image src="images/thumb_lungs_256.png" alt="lungs_256" thumbnail />
-                  </Button>
-                </Col>
-
-              </Row>
-            </Container>
-
-          </Modal.Body>
+          <Modal.Title>
+            Load demo data
+          </Modal.Title>
         </Modal.Header>
+        <Modal.Body>
+
+          <Container>
+            <Row>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton0} >
+                  <Image src="images/thumb_lungs.png" alt="lungs" thumbnail />
+                </Button>
+              </Col>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton1} >
+                  <Image src="images/thumb_brain.png" alt="brain" thumbnail />
+                </Button>
+              </Col>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton2} >
+                  <Image src="images/thumb_gm3_512_512_165.png" alt="gm3" thumbnail />
+                </Button>
+              </Col>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton3} >
+                  <Image src="images/thumb_woman_pelvis.png" alt="woman_pelvis" thumbnail />
+                </Button>
+              </Col>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton4} >
+                  <Image src="images/thumb_ocb.png" alt="woman_pelvis" thumbnail />
+                </Button>
+              </Col>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton5} >
+                  <Image src="images/thumb_ct_256.png" alt="ct_256" thumbnail />
+                </Button>
+              </Col>
+              <Col xs={6} md={4}>
+                <Button variant="light" onClick={this.onButton6} >
+                  <Image src="images/thumb_lungs_256.png" alt="lungs_256" thumbnail />
+                </Button>
+              </Col>
+
+            </Row>
+          </Container>
+
+        </Modal.Body>
       </Modal>
     return jsxModalDemo;
   } // end render
