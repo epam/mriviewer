d52529fee9e046007886c7c4f1997e37328f8e37
Oleg Sidorkin <oleg_sidorkin@epam.com>
2021-05-17 23:59:48 +0300

    index file updated

--- src/index.js
+++ src/index.js
@@ -5,19 +5,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
 
-import rootReducer from './store/Store';
 import App from './app/App';
 
 import './index.css';
 
-const rootElement = document.getElementById('root');
-
-const store = createStore(rootReducer);
-
-ReactDOM.render(<Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement);
+ReactDOM.render(<App />,  document.getElementById('root'));
