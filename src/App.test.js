43ab36933baada36deb97f3c6415cdd697938861
Oleg Sidorkin <oleg_sidorkin@epam.com>
2021-08-29 13:08:35 +0300

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,27 @@
-// const puppeteer = require('puppeteer');
-//
-// const isDebugging = () => {
-// 	return process.env.DEBUG === 'true' ? {
-// 		headless: false,
-// 		slowMo: 250,
-// 		devtools: true
-// 	} : {};
-// }
-//
-// let browser
-// let page
-// beforeAll(async() => {
-// 	browser = await puppeteer.launch(isDebugging())
-// 	page = await browser.newPage()
-// 	await page.goto('http://localhost:3000/')
-// 	await page.setViewport({ width: 500, height: 2400 })
-// })
-//
-// describe('on page load', () => {
-// 	test('loads correct title', async() => {
-// 		const pageTitle = await page.title();
-//
-// 		expect(pageTitle).toBe('Med3Web DICOM 2D/3D browser')
-// 	}, 10000)
-// })
+const puppeteer = require('puppeteer');
+
+const isDebugging = () => {
+	return process.env.DEBUG === 'true' ? {
+		headless: false,
+		slowMo: 250,
+		devtools: true
+	} : {};
+}
+
+let browser
+let page
+beforeAll(async() => {
+	browser = await puppeteer.launch(isDebugging())
+	page = await browser.newPage()
+	await page.goto('http://med3web.s3-website.eu-central-1.amazonaws.com/')
+	await page.setViewport({ width: 1280, height: 1024 })
+})
+
+describe('on page load', () => {
+	test('loads correct title', async() => {
+		const pageTitle = await page.title();
+
+		expect(pageTitle).toBe('Med3Web DICOM 2D/3D browser')
+	}, 10000)
+})
 
