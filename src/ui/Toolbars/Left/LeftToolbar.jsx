9dd165259ab91205e50aa089ff600a53119d8f90
Nikita_Brovkovich <Nikita_Brovkovich@epam.com>
2022-03-01 17:25:01 +0400

    Setup tests.

--- src/ui/Toolbars/Left/LeftToolbar.jsx
+++ src/ui/Toolbars/Left/LeftToolbar.jsx
@@ -1,16 +1,16 @@
 /*
- * Copyright 2021 EPAM Systems, Inc. (https://www.epam.com/)
+ * Copyright 2022 EPAM Systems, Inc. (https://www.epam.com/)
  * SPDX-License-Identifier: Apache-2.0
  */
 
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import ViewMode from '../../store/ViewMode';
-import { ModeSwitcherToolbar } from './Left/ModeSwitcherToolbar';
-import { ModeFast3dToolbar } from './Left/ModeFast3dToolbar';
-import ModeView from '../../store/ViewMode';
-import { Mode2dToolbar } from './Left/Mode2dToolbar';
+import ViewMode from '../../../store/ViewMode';
+import { ModeSwitcherToolbar } from './ModeSwitcherToolbar';
+import { ModeFast3dToolbar } from './ModeFast3dToolbar';
+import ModeView from '../../../store/ViewMode';
+import { Mode2dToolbar } from './Mode2dToolbar';
 
 export const LeftToolbar = () => {
   const { viewMode } = useSelector((state) => state);
